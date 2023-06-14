8c539a6615cbb208bf3708f17d7a3674815fdbeb
Mariugo Megiani <mariugomegiani@gmail.com>
2023-06-14 10:55:52 -0300

    refactor to use on web

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:pontaagro/config/router/farms_page_router.dart';
 import 'package:pontaagro/config/theme/app_colors.dart';
 import 'package:pontaagro/config/theme/app_text_theme.dart';
 import 'package:pontaagro/core/bindings/app_bindings.dart';
+import 'package:pontaagro/features/farm_animals/presentation/pages/add_animals_page.dart';
+import 'package:pontaagro/features/farm_animals/presentation/pages/farm_animals_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,26 +15,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBindings(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Ponta >>',
-        theme: ThemeData(
-          textTheme: AppTextTheme.lightTextTheme,
-          useMaterial3: true,
-          colorScheme: lightColorScheme,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        darkTheme: ThemeData(
-          textTheme: AppTextTheme.darkTextTheme,
-          useMaterial3: true,
-          colorScheme: darkColorScheme,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        routes: {
-          '/': (context) => FarmsPageRouter.page,
-        },
+    // return AppBindings(
+    //   child:
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Ponta >>',
+      theme: ThemeData(
+        textTheme: AppTextTheme.lightTextTheme,
+        useMaterial3: true,
+        colorScheme: lightColorScheme,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      darkTheme: ThemeData(
+        textTheme: AppTextTheme.darkTextTheme,
+        useMaterial3: true,
+        colorScheme: darkColorScheme,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const AddAnimalPage(),
+      // routes: {
+      //   '/': (context) => FarmsPageRouter.page,
+      // },
+      // ),
     );
   }
 }
