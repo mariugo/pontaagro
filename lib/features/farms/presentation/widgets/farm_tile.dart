b5b8637165876c2db16b190f51f53d63cdef0f5c
Mariugo Megiani <mariugomegiani@gmail.com>
2023-06-14 07:13:53 -0300

    Farm tile refactor

--- lib/features/farms/presentation/widgets/farm_tile.dart
+++ lib/features/farms/presentation/widgets/farm_tile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:pontaagro/core/extensions/size_extensions.dart';
 import 'package:pontaagro/features/farms/data/models/farms_model.dart';
 
 class FarmTile extends StatelessWidget {
@@ -16,46 +18,53 @@ class FarmTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      child: Card(
-        color: Theme.of(context).colorScheme.primaryContainer,
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: ListTile(
-            title: Text(
-              farm.name,
-              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  color: Theme.of(context).colorScheme.onPrimaryContainer),
-            ),
-            subtitle: Text(
-              'Animais: ${farm.quantity}',
-              style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                  color: Theme.of(context).colorScheme.onPrimaryContainer),
-            ),
-            trailing: SizedBox(
-              width: 190,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: onEdit,
-                    child: const Text('Editar'),
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  TextButton(
-                    onPressed: onDelete,
-                    child: Text(
-                      'Excluir',
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodySmall!
-                          .copyWith(color: Theme.of(context).colorScheme.error),
-                    ),
-                  ),
-                ],
+      onTap: () {},
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          child: FaIcon(
+            FontAwesomeIcons.tractor,
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+        ),
+        title: Text(
+          farm.name,
+          style: Theme.of(context)
+              .textTheme
+              .bodyMedium!
+              .copyWith(color: Theme.of(context).colorScheme.primary),
+        ),
+        subtitle: Text(
+          'Animais: ${farm.quantity}',
+          style: Theme.of(context).textTheme.bodySmall!.copyWith(
+              color: Theme.of(context).colorScheme.onPrimaryContainer),
+        ),
+        trailing: SizedBox(
+          width: context.percentWidth(0.4),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: onEdit,
+                child: Text(
+                  'Editar',
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
               ),
-            ),
+              const SizedBox(
+                width: 5,
+              ),
+              TextButton(
+                onPressed: onDelete,
+                child: Text(
+                  'Excluir',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodySmall!
+                      .copyWith(color: Theme.of(context).colorScheme.error),
+                ),
+              ),
+            ],
           ),
         ),
       ),
